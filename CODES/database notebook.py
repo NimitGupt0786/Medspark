4db1e70a2685cfd52ec71680e4306c831ec1e002
Nimit_Gupta <110985866+NimitGupt0786@users.noreply.github.com>
2025-08-03 20:39:36 +0530

    Update database notebook.py

---
+++ CODES/database notebook.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC
+# MAGIC create catalog cars_catalog
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema cars_catalog.slver
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema cars_catalog.gold
+
+# COMMAND ----------
